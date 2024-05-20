2cc134f19c796847eea66fd666adb1bcd6dc1745
luanspataro <luan.spataro@hotmail.com>
2024-05-19 23:12:52 -0300

    Desafio ModalGR finalizado

---
+++ NomeCracha/Program.cs
@@ -0,0 +1,38 @@
+﻿string[] listaNomes = new string[150];
+string[] palavrasRemover = { "de", "da", "das", "do", "dos", "e" };
+string nome, ultimoSobrenome;
+int tamanho;
+
+for (int i = 0; i < 150; i++)
+{
+    // Resetando as iniciais para não acumular no próximo loop
+    string iniciais = "";
+
+    Console.WriteLine("Digite o nome do colaborador: ");
+    nome = Console.ReadLine();
+
+    // Checando se as palavras proibidas aparecem no nome
+    foreach (string palavra in palavrasRemover)
+    {
+        if (nome.Contains(palavra))
+        {
+            nome = nome.Replace($" {palavra} ", " ");
+        }
+    }
+
+    // Separando o nome em uma array
+    string[] partesNome = nome.Split(' ');
+    ultimoSobrenome = partesNome.Last() + ", ";
+    tamanho = partesNome.Length;
+
+    // Pegando as iniciais do nome e sobrenome
+    for (int j = 0; j < tamanho - 1; j++)
+    {
+        iniciais += partesNome[j].Substring(0, 1) + ". ";
+    }
+
+    listaNomes[i] = ultimoSobrenome + iniciais;
+
+    // Saída do nome formatado
+    Console.WriteLine(listaNomes[i]);
+}
---
+++ SepararDados/Program.cs
@@ -0,0 +1,41 @@
+﻿string dadosBrutos;
+double numerico;
+string[] dadosSeparados;
+List<string> listaTexto = new List<string>();
+List<double> listaNumerico = new List<double>();
+
+Console.WriteLine("Digite os dados: ");
+dadosBrutos = Console.ReadLine();
+
+dadosSeparados = dadosBrutos.Split(',');
+
+// Loop para pegar cada dado isoladamente
+foreach (string dado in dadosSeparados)
+{
+    // Try Catch para tentar transformar o dado em número (em caso de erro devolve em string)
+    // Usando o trim para remover os espaços entre as vírgulas
+    try
+    {
+        numerico = double.Parse(dado.Trim());
+        listaNumerico.Add(numerico);
+    }
+    catch
+    {
+        listaTexto.Add(dado.Trim());
+    }
+}
+
+// Saída dos dados organizados
+Console.WriteLine("Lista de Strings: ");
+foreach (string texto in listaTexto)
+{
+    Console.WriteLine(texto);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Lista de Números: ");
+foreach (double numero in listaNumerico)
+{
+    Console.WriteLine(numero);
+}
